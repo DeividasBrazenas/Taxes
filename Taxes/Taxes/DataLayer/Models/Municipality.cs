6a40a60f7d6a3f4f93d86e60b8a95b6d0e1573c8
Deividas <deividas.brazenas@gmail.com>
2019-05-30 17:52:31 +0300

    Removing unused references from models

--- Taxes/Taxes/DataLayer/Models/Municipality.cs
+++ Taxes/Taxes/DataLayer/Models/Municipality.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Taxes.Service.DataLayer.Models
+﻿namespace Taxes.Service.DataLayer.Models
 {
     public class Municipality : BaseModel
     {

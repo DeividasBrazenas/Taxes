0da4e5195dffb336b604e11feaafc4cd36ca61ca
Deividas <deividas.brazenas@gmail.com>
2019-05-31 00:37:52 +0300

    Adding DateTime extension

---
+++ Taxes/Taxes/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taxes.Service.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsBewteenTwoDates(this DateTime dt, DateTime start, DateTime end)
+        {
+            return dt >= start || dt <= end;
+        }
+    }
+}

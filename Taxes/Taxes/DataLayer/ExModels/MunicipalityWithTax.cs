54fc19bad9f7e93df4b2f0233134e8d7cd8c1d26
Deividas <deividas.brazenas@gmail.com>
2019-05-30 23:26:25 +0300

    Updating extension model

--- Taxes/Taxes/DataLayer/ExModels/MunicipalityWithTax.cs
+++ Taxes/Taxes/DataLayer/ExModels/MunicipalityWithTax.cs
@@ -4,6 +4,13 @@ namespace Taxes.Service.DataLayer.ExModels
 {
     public class MunicipalityWithTax : Municipality
     {
+        public MunicipalityWithTax(Municipality municipality, double taxValue)
+        {
+            Municipality = municipality;
+            TaxValue = taxValue;
+        }
+
+        public Municipality Municipality { get; set; }
         public double TaxValue { get; set; }
     }
 }

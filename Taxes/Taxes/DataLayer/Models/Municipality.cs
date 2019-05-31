18bcf2e901bbbe8e8fb7430c377cea3461bf426d
Deividas <deividas.brazenas@gmail.com>
2019-06-01 00:43:01 +0300

    Updating models

--- Taxes/Taxes/DataLayer/Models/Municipality.cs
+++ Taxes/Taxes/DataLayer/Models/Municipality.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Taxes.Service.DataLayer.Models
 {
     public class Municipality : BaseModel
     {
+        [Required]
         public string Name { get; set; }
         public ICollection<Tax> Taxes { get; set; }
     }
--- Taxes/Taxes/DataLayer/Models/Tax.cs
+++ Taxes/Taxes/DataLayer/Models/Tax.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Taxes.Service.DataLayer.Models
 {
     public class Tax : BaseModel
     {
+        [Required]
         public int MunicipalityId { get; set; }
+
+        [Required]
         public Municipality Municipality { get; set; }
+
+        [Required]
         public TaxFrequency Frequency { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
+
+        [Required]
         public double Value { get; set; }
     }
 

18854522b6643f5e8fad3b675b3dd3036bbe69be
Deividas Brazenas <deividas.brazenas@gmail.com>
2019-06-03 17:55:12 +0300

    Adding integration tests

---
+++ Taxes/Taxes.Tests/IntegrationTests/Helpers/TestClientProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Taxes.Service;
+
+namespace Taxes.Tests.IntegrationTests.Helpers
+    {
+    public class TestClientProvider
+        {
+            public HttpClient Client { get; }
+
+            public TestClientProvider()
+            {
+                var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+                Client = server.CreateClient();
+            }
+        }
+    }
---
+++ Taxes/Taxes.Tests/IntegrationTests/MunicipalityTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using Taxes.Service.DataLayer.Models;
+using Taxes.Tests.IntegrationTests.Helpers;
+
+namespace Taxes.Tests.IntegrationTests
+    {
+    [TestFixture]
+    public class MunicipalityTests
+    {
+        private HttpClient Client;
+
+            [SetUp]
+            public void Init()
+            {
+                Client = new TestClientProvider().Client;
+            }
+
+            [Test]
+            public async Task GetMunicipality_Succeeds()
+            {
+                var response = await Client.GetAsync("/odata/municipalities");
+            }
+        }
+    }

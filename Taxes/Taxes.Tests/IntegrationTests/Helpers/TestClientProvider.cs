c56bbcd374b65e5e465d9dca3bcec397074df610
Deividas <deividas.brazenas@gmail.com>
2019-06-03 20:00:40 +0300

    Updating test client

--- Taxes/Taxes.Tests/IntegrationTests/Helpers/TestClientProvider.cs
+++ Taxes/Taxes.Tests/IntegrationTests/Helpers/TestClientProvider.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Taxes.Service;
 
 namespace Taxes.Tests.IntegrationTests.Helpers
-    {
+{
     public class TestClientProvider
+    {
+        public HttpClient Client { get; }
+
+        public TestClientProvider()
         {
-            public HttpClient Client { get; }
+            var configuration = InitConfiguration();
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>().UseSetting("ConnectionStrings:DefaultConnection", configuration.GetConnectionString("DefaultConnection")));
 
-            public TestClientProvider()
-            {
-                var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = server.CreateClient();
+        }
 
-                Client = server.CreateClient();
-            }
+        public static IConfiguration InitConfiguration()
+        {
+            var directory =  Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"));
+            return new ConfigurationBuilder().AddJsonFile(directory + "appsettings.tests.json").Build();
         }
     }
+}
